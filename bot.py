373820d5561d8770034badf110ddb84c1eff568a
Fedor <58223445+Marginalisimus@users.noreply.github.com>
2021-01-26 18:04:39 +0300

    problem

--- bot.py
+++ bot.py
@@ -5,6 +5,10 @@ from time import time
 import config
 from datetime import date
 from datetime import time
+from sortedcontainers import SortedList
+
+from utilityfunc import *
+from classes import *
 
 intents = discord.Intents.all()
 
@@ -15,72 +19,11 @@ time_started = None
 lesson_time = None
 lessons_month = 0
 
-class lesson_month:
-    lessons = []
-
-    def __init__(self, lessons):
-        self.lessons = lessons
-
-    def sort(self):
-        for i in range(1, len(self.lessons)):
-            el = self.lessons[i]
-            j = i
-            while j > 0 and self.lessons[j - 1] > el:
-                self.lessons[j] = self.lessons[j - 1]
-                j -= 1
-            self.lessons[j] = el
-
-    def insert(self, lesson_day):
-        self.lessons.append(lesson_day)
-        self.sort()
-
-class lesson_day:
-    def __init__(self, date, lesson_count, lesson_time):
-        self.date = date
-        self.lesson_count = lesson_count
-        self.lesson_time = time
-
-    def __str__(self):
-        word = 'занятия'
-        if lesson_count == 1:
-            word = 'занятие'
-        if lesson_count == 0:
-            word = 'занятий'    
-        
-        return f'{self.date.day}.{self.date.month}.{self.date.year}: {self.lesson_count} {word} ({self.lesson_time})'
-
-    def __lt__(self, other):
-        return self.date.day < other.date.day
-    def __gt__(self, other):
-        return self.date.day > other.date.day      
-
 def get_channel(channel_id):
     return client.get_guild(config.server_id).get_channel(channel_id)
     
 async def get_message(msg_id):
     return await client.get_guild(config.server_id).get_channel(config.bot_channel).fetch_message(msg_id)
-
-def get_day(data):
-    data = re.search('(\d\d).(\d\d).(\d\d\d\d): (\d) заняти[яей] \((\d):(\d\d):(\d\d)\)', data)
-    day = lesson_day(date(int(data[1]), int(data[2]), int(data[3])), int(data[4]), time(int(data[5]), int(data[6]), int(data[7])))
-    return day
-
-async def get_lessons(msg_id=config.main_post_id):
-    lessons = []
-    msg = await get_message(msg_id)
-    data = msg.content
-    data = data.split('\n')
-
-    data[0] = re.search('^Занятия (\d\d).\d\d\d\d \(всего (\d+)\):', data[0])
-    if int(data[0][1]) != date.month:
-        msg.channel.send(msg.content)
-        d = date.strftime('%m.%y')
-        msg.edit(f'Занятия {d} (всего 0):')
-    lessons_month = int(data[0][2])
-    data.pop(1)
-    for i in data[1:]:
-        lessons.append(get_day[i])
-    return lessons    
 
 @client.event
 async def on_ready():
@@ -91,21 +34,29 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
-    if message.content.startswith('!post'):
-        await message.channel.send(f'3')
-    if message.content.startswith('!clear'): 
+    #if message.content.startswith('$hello'):
+        #await message.channel.send('Hello!')
+    #if message.content.startswith('!post'):
+        #await message.channel.send(f'3')
+    #if message.content.startswith('!clear'): 
+        #msg = await get_message(config.main_post_id)
+        #await msg.edit(content='Занятия 01.21 (всего 3)')
+    if message.content.startswith('!add'):
+        print(message.content) 
+        data = re.search('!add (\d\d) (\d):(\d\d):(\d\d)', message.content)
+        print(data)
         msg = await get_message(config.main_post_id)
-        await msg.edit(content='Занятия 01.21 (всего 3)')
-    if message.content.startswith('!add'): 
 
-        data = re.search('^!add (\d\d) (\d):(\d\d):(\d\d)', message.content)
-        msg = await get_message(config.main_post_id)
-        lessons = lesson_month(get_lessons())
+        lessons = SortedList(get_lessons())
+
+        lesson_count = await count_lessons(int(data[2]), int(data[3]))
+        lesson_date = date(date.year, date.month, int(data[1]))
+        lesson_time = time(int(data[2]), int(data[3]), int(data[4]))
+
+        new_lesson = lesson_day(lesson_date, lesson_count, lesson_time)
+
+        lessons.add(new_lesson)
         
-        new_lesson = lesson_day
-
         await msg.edit(content=msg.content + f'\n {int(data[1])}.{int(data[2])}: занятие {int(data[3])} минут')
 
 @client.event
--- utilityfunc.py
+++ utilityfunc.py
@@ -5,34 +5,48 @@ from time import time
 import config
 from datetime import date
 from datetime import time
+from sortedcontainers import SortedList
 
-from .classes import lesson_month
-from .classes import lesson_day
+from classes import lesson_day
+from bot import get_message
+from bot import get_channel
 
-def get_channel(channel_id):
-    return client.get_guild(config.server_id).get_channel(channel_id)
-    
-async def get_message(msg_id):
-    return await client.get_guild(config.server_id).get_channel(config.bot_channel).fetch_message(msg_id)
+def to_message(lesson_month: SortedList):
+    start = f'Занятие {lesson_month[0].date.month}.{lesson_month[0].date.year} (всего {len(lesson_month)}):'
+
+    for lesson in lesson_month:
+        start += '\n'
+        start += str(lesson)
+
+    return start    
+
+def count_lessons(hours, minutes):
+    minutes += hours * 60
+    lessons = minutes/90
+    lessons = round(lessons)
+    return lessons
 
 def get_day(data):
     data = re.search('(\d\d).(\d\d).(\d\d\d\d): (\d) заняти[яей] \((\d):(\d\d):(\d\d)\)', data)
     day = lesson_day(date(int(data[1]), int(data[2]), int(data[3])), int(data[4]), time(int(data[5]), int(data[6]), int(data[7])))
     return day
 
+
 async def get_lessons(msg_id=config.main_post_id):
     lessons = []
     msg = await get_message(msg_id)
-    data = msg.content
-    data = data.split('\n')
+    data = msg.content.split('\n')
+   # print(data)
 
-    data[0] = re.search('^Занятия (\d\d).\d\d\d\d \(всего (\d+)\):', data[0])
-    if int(data[0][1]) != date.month:
+    data_re = re.search('Lessons (\d\d).\d\d \(total (\d)\)', data[0])
+    #print(data_re[0])
+    #print(data_re[1])
+    #print(data_re[2])
+    if int(data_re[1]) != date.month:
         msg.channel.send(msg.content)
         d = date.strftime('%m.%y')
-        msg.edit(f'Занятия {d} (всего 0):')
-    lessons_month = int(data[0][2])
-    data.pop(1)
-    for i in data[1:]:
-        lessons.append(get_day[i])
+        msg.edit(f'Lessons {d} (total 0):')
+    lessons_month = int(data_re[2])
+    #for i in data[1]:
+     #   lessons.append(get_day[i])
     return lessons    
